d145142bdfe6114424c2d4ee9de3d7856360096f
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to add a custom bar button item to the popup menu
+
+
+This example demonstrates how to add a custom bar button item to the popup menu.<br><br>To add a new item to a popup menu shown for the navigation pane of  PDF Viewer, handle the <a href="https://documentation.devexpress.com/#WindowsForms/DevExpressXtraPdfViewerPdfViewer_PopupMenuShowingtopic">PdfViewer.PopupMenuShowing</a> event and then use the <a href="https://documentation.devexpress.com/#WindowsForms/DevExpressXtraPdfViewerPdfPopupMenuShowingEventArgs_ItemLinkstopic">PdfPopupMenuShowingEventArgs.ItemLinks </a> property. <br><br><br>
+
+<br/>
+
+
---
+++ VB/CustomPopupMenu/Form1.vb
@@ -0,0 +1,30 @@
+﻿Imports System.Windows.Forms
+Imports DevExpress.XtraPdfViewer
+Imports DevExpress.XtraBars
+
+Namespace CustomPopupMenu
+    Partial Public Class Form1
+        Inherits Form
+
+        Public Sub New()
+            InitializeComponent()
+            pdfViewer1.LoadDocument("..\..\Demo.pdf")
+        End Sub
+
+        Private Sub pdfViewer1_PopupMenuShowing(ByVal sender As Object, ByVal e As PdfPopupMenuShowingEventArgs) Handles pdfViewer1.PopupMenuShowing
+            ' Create a bar button item.
+            Dim browseBarButton As New BarButtonItem()
+            browseBarButton.Caption = "Custom Item"
+
+            ' Insert the bar buttom item into the PDF Viewer popup menu and start a new group.
+            e.ItemLinks.Add(browseBarButton, True)
+
+            ' Handle the bar button click event.
+            AddHandler browseBarButton.ItemClick, AddressOf browseBarButton_ItemClick
+        End Sub
+
+        Private Sub browseBarButton_ItemClick(ByVal sender As Object, ByVal e As ItemClickEventArgs)
+            MessageBox.Show("ItemClick event fires")
+        End Sub
+    End Class
+End Namespace
